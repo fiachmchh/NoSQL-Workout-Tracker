569e695eab05d1203c67b04f2595d175bc7266d8
fiachmchh <fiachmchh@gmail.com>
2021-03-12 18:59:56 -0500

    server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,1 +1,34 @@
 //require routes in here
+
+// const mongojs = require("mongojs");
+const mongoose = require("mongoose");
+const express = require("express");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//change mongodb://localhost/"" to my database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
